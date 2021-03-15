f6a5130e4ee4dc2988fb2ad214ba91b8459991c8
annkohnovich <annkohnovich@gmail.com>
2021-03-15 17:26:56 +0300

    Contact creation test

---
+++ addressbook-web-tests3/src/test/java/ru/stqa/ptf/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.ptf.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String lastName;
+    private final String mobilePhone;
+    private final String email;
+
+    public ContactData(String first_name, String last_name, String mobile_phone, String email) {
+        this.firstName = first_name;
+        this.lastName = last_name;
+        this.mobilePhone = mobile_phone;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}

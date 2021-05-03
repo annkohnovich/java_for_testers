f4b8c0377243b9d9130327ba003ccf2c9d1679b8
annkohnovich <annkohnovich@gmail.com>
2021-05-03 14:16:36 +0200

    Max id for GroupCreationTest (using Comparator)

--- addressbook-web-tests3/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreation.java
+++ addressbook-web-tests3/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreation.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.ptf.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,17 +22,15 @@ public class GroupCreation extends TestBase {
         List <GroupData> after = app.getGroupHelper().getGroupList ();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (GroupData g : after) {
-            if (g.getId()>max) {
-                max = g.getId();
+        Comparator<? super GroupData> byId= new Comparator<GroupData>() {
+            @Override
+            public int compare(GroupData o1, GroupData o2) {
+                return Integer.compare(o1.getId(), o2.getId());
             }
-        }
+        };
+        int max = after.stream().max(byId).get().getId();
         group.setId(max);
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
-
-
   }
-
 }

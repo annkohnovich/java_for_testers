269c53c14646f3617c42d06bccb81420ca22142a
annkohnovich <annkohnovich@gmail.com>
2021-08-03 14:54:30 +0200

    DataGenerator with parameters (6.3)

--- addressbook-web-tests3/src/test/java/ru/stqa/ptf/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests3/src/test/java/ru/stqa/ptf/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.ptf.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.ptf.addressbook.model.GroupData;
 
 import java.io.File;
@@ -10,15 +13,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File pathToFile = new File(args[1]);
+    @Parameter (names = "-c", description = "Group Count")
+    public int count;
 
-        List <GroupData> groups = generateGroups(count);
-        save(groups, pathToFile);
+    @Parameter (names = "-f", description = "Target File")
+    public String pathToFile;
+
+
+    public static void main(String[] args) throws IOException {
+        GroupDataGenerator generator = new GroupDataGenerator();
+        JCommander commander = new JCommander(generator);
+        try {
+            commander.parse(args);
+        } catch (ParameterException ex)
+        {
+            commander.usage();
+            return;
+        }
+        generator.run();
     }
 
-    private static void save(List<GroupData> groups, File file) throws IOException {
+    private void run() throws IOException {
+        List <GroupData> groups = generateGroups(count);
+        save(groups, new File(pathToFile));
+    }
+
+    private void save(List<GroupData> groups, File file) throws IOException {
         Writer writer = new FileWriter(file);
         for (GroupData group :groups) {
             writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
@@ -26,7 +46,7 @@ public class GroupDataGenerator {
         writer.close();
     }
 
-    private static List<GroupData> generateGroups(int count) {
+    private List<GroupData> generateGroups(int count) {
         List <GroupData> groups = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             groups.add(new GroupData().withName(String.format("test %s", i))

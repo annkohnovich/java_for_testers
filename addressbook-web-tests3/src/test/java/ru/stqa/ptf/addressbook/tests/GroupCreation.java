8f25b43e3db1fd75ae0abd000c32acbc3a6e01df
annkohnovich <annkohnovich@gmail.com>
2021-08-03 17:00:34 +0200

    DataProvider for GroupCreationTest (6.4)

--- addressbook-web-tests3/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreation.java
+++ addressbook-web-tests3/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreation.java
@@ -5,16 +5,27 @@ import ru.stqa.ptf.addressbook.model.GroupData;
 import ru.stqa.ptf.addressbook.model.Groups;
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreation extends TestBase {
 
-    @Test
-  public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[]{new GroupData().withName("name").withHeader("header").withFooter("footer")});
+        list.add(new Object[]{new GroupData().withName("name2").withHeader("header2").withFooter("footer2")});
+        return list.iterator();
+    }
+
+    @Test (dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test2");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().all();

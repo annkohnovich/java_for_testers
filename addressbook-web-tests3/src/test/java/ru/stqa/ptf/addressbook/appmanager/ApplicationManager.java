25f0057a0779241aba34804590f6dfd8d6ebea1a
annkohnovich <annkohnovich@gmail.com>
2021-09-13 16:46:44 +0200

    Selenium Server configuration for GroupTests (FF browser)

--- addressbook-web-tests3/src/test/java/ru/stqa/ptf/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests3/src/test/java/ru/stqa/ptf/addressbook/appmanager/ApplicationManager.java
@@ -1,15 +1,19 @@
 package ru.stqa.ptf.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -32,13 +36,20 @@ public class ApplicationManager {
         dbHelper = new DbHelper();
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+        if ("".equals(properties.getProperty("selenium.server"))){
+        }
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
         } else if (browser.equals(BrowserType.IE)) {
             wd = new InternetExplorerDriver();
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+            capabilities.setBrowserName(browser);
         }
+        
 
 
         wd.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);

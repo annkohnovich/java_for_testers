15440db4b001fd061e6ff6eee900ac743b67d3d3
annkohnovich <annkohnovich@gmail.com>
2021-10-28 11:28:07 +0200

    Revert "conmmit 6"
    This reverts commit b29e1bd95fcc698f1272cb69179c5f843fd0613b.

--- bygify-tests/src/test/java/ru/stqa/pft/bugify/test/TestBase.java
+++ bygify-tests/src/test/java/ru/stqa/pft/bugify/test/TestBase.java
@@ -35,7 +35,6 @@ public class TestBase {
         } else return true;
     }
 
-    
     public boolean isIssueExist(int issueId) throws Exception {
         Response response = RestAssured.get("https://bugify.stqa.ru/api/issues/" + issueId + ".json");
         if (!(response.statusCode() == 404)) {
